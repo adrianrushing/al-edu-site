75877be27c9707644130c62bbc68d7fb86a6342b
Adrian Rushing <adrianjrushing@gmail.com>
2026-02-12 20:00:16 -0600

    add docker containers

--- apps/api/app/config.py
+++ apps/api/app/config.py
@@ -2,4 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
-    DEBUG = True
+    DEBUG = os.getenv("FLASK_DEBUG", "1") == "1"
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql://postgres:postgres@db:5432/dashboard",
+    )
